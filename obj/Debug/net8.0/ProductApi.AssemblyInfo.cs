ae21ad8f06aee97e528e82e27fd865defd73eff7
Fatma Yılmaz <79848362+fatmayilmaz4101@users.noreply.github.com>
2025-01-15 13:42:32 +0300

    .

--- obj/Debug/net8.0/ProductApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/ProductApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+803bcc910927c327c896e056775a9ca2f61c5cda")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eee47e93a9300e7182a27ee1a17733dff6db5e48")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
